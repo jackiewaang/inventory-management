1fe335b3257c780eaadc59c52b4f920a52c260ff
jackiewaang <zjackiewang03@gmail.com>
2026-07-23 16:39:40 +0200

    fix: forgot request

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 
